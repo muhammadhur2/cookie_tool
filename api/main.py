34edb380539e44d98ed9cd43743f1d1e34c69b63
CommanderAta <commanderata@gmail.com>
2024-01-01 04:02:13 +0500

    wow

--- api/main.py
+++ api/main.py
@@ -6,23 +6,18 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Get data from form
         secret_key = request.form['secret_key']
         user_id = request.form['user_id']
 
-        # Set the secret key and create a session
         app.secret_key = secret_key
         with app.test_request_context('/'):
             session = {'user_id': user_id}
             
-            # Serialize the session into a cookie
             cookie = SecureCookieSessionInterface().get_signing_serializer(app).dumps(session)
 
-            # Show the generated cookie
-            response = make_response(f"Generated Cookie: {cookie}")
-            return response
+            # Render the cookie display template with the generated cookie
+            return render_template('cookie_display.html', cookie=cookie)
 
-    # Render the HTML form
     return render_template('index.html')
 
 if __name__ == '__main__':
